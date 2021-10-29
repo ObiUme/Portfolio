65dd0649c067169733ae67a3132ec22586347af6
Obi <mr.obiume@gmail.com>
2021-10-28 19:39:35 -0700

    user routes' '

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+    
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def update
+        user = User.find_by(id: session[:user_id])
+        user.update!(user_params)
+        render json: user, status: :accepted
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :is_admin )
+    end
+end
